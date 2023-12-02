89745d6880d416a0245a04da1ff406d759a66308
Alexandr Gulinsky <swith0711@gmail.com>
2023-12-02 19:08:37 +0200

    meta

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import { BrowserRouter } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename='goit-react-hw-05-movies'>
-      <App />
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -6,7 +6,7 @@ import { useLocation, useSearchParams } from "react-router-dom"
 
 // emotion
 import { Container, Ul, LinkStyled } from "./Movies.styled"
-import { Helmet } from "react-helmet"
+import { Helmet } from "react-helmet-async"
 
 const Movies = () => {
 
@@ -47,8 +47,7 @@ const Movies = () => {
 
   return (
     <>
-      <Helmet>
-        <title>Title | Movie Page</title>
+        {/* <title>Title | Movie Page</title>
         <meta property="og:url" content="https://a-gulinsky.github.io/goit-react-hw-05-movies/movies" />
         <meta property="og:title" content="Title | Movie Page" />
         <meta property="og:site_name" content="Movies" />
@@ -57,7 +56,11 @@ const Movies = () => {
         <meta property="og:locale" content="en_US" />
         <meta property="og:image" content="https://img.freepik.com/free-photo/fresh-natural-green-schefflera-arboricola-background_53876-153311.jpg?w=996&t=st=1701474143~exp=1701474743~hmac=50682caf3fdfd630dff9ea401437b10bd57f30234e16b1f53a14a718a5ea0498" />
         <meta name="description" content="Description: Best Movie in Ukraine 2. Best Movie in Ukraine 3. Best Movie in Ukraine"/>
-        <link rel="canonical" href="https://a-gulinsky.github.io/goit-react-hw-05-movies/movies" />
+        <link rel="canonical" href="https://a-gulinsky.github.io/goit-react-hw-05-movies/movies" /> */}
+      <Helmet>
+          <title>Movies</title>
+          <meta name="description" content="Movies to enjoy your day"/>
+          <link rel="canonical" href="/movies" />
       </Helmet>
 
       <Container>
