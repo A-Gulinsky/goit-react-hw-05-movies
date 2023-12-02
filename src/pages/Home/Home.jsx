774b8d48212155856aa79d39859fa7648b1590fd
Alexandr Gulinsky <swith0711@gmail.com>
2023-12-02 15:04:45 +0200

    add meta

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,6 +6,7 @@ import { useLocation } from "react-router-dom"
 
 // emotion
 import { Title, Ul, LinkStyled } from "./Home.styled"
+import { Helmet } from "react-helmet"
 
 const Home = () => {
 
@@ -28,6 +29,16 @@ const Home = () => {
 
   return (
     <>
+      <Helmet>
+        <title>Title | Home Page</title>
+        <meta property="og:url" content="https://a-gulinsky.github.io/goit-react-hw-05-movies" />
+        <meta property="og:title" content="Title | Movie Page" />
+        <meta property="og:site_name" content="Movies" />
+        <meta property="og:type" content="article" />
+        <meta property="og:description" content="Description: Best Movie in Ukraine 2. Best Movie in Ukraine 3. Best Movie in Ukraine" />
+        <meta property="og:locale" content="en_US" />
+      </Helmet>
+      
       <Title>Trending Today</Title>
 
       <Ul>
