01806ce187aa461b448632bc3e0f27dec571e14d
Alexandr Gulinsky <swith0711@gmail.com>
2023-12-02 17:30:40 +0200

    Update Movies.jsx

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -48,7 +48,15 @@ const Movies = () => {
   return (
     <>
       <Helmet>
-        <title>Movies</title>
+        <title>Title | Movie Page</title>
+        <meta property="og:url" content="https://a-gulinsky.github.io/goit-react-hw-05-movies/movies" />
+        <meta property="og:title" content="Title | Movie Page" />
+        <meta property="og:site_name" content="Movies" />
+        <meta property="og:type" content="article" />
+        <meta property="og:description" content="Description: Best Movie in Ukraine 2. Best Movie in Ukraine 3. Best Movie in Ukraine" />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:image" content="https://img.freepik.com/free-photo/fresh-natural-green-schefflera-arboricola-background_53876-153311.jpg?w=996&t=st=1701474143~exp=1701474743~hmac=50682caf3fdfd630dff9ea401437b10bd57f30234e16b1f53a14a718a5ea0498" />
+        <meta name="description" content="Description: Best Movie in Ukraine 2. Best Movie in Ukraine 3. Best Movie in Ukraine"/>
       </Helmet>
 
       <Container>
